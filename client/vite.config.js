3e02f3744d08b0d51e31e32f03db16076388c32c
Puneeth Regonda <puneethregonda1291@gmail.com>
2024-12-08 12:43:57 -0800

    added listing changes

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,13 +3,16 @@ import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: '../backend/src/main/webapp', // Build output directory matches the backend's static file location
+    emptyOutDir: true,
+  },
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:8080', // Backend server URL
+        target: 'http://localhost:8080', // Backend URL during local dev
         changeOrigin: true,
-        secure: false, // Use true only if backend uses HTTPS with valid certs
-        rewrite: (path) => path.replace(/^\/api/, '/api'), // Ensures the API path is preserved
+        secure: false,
       },
     },
   },
