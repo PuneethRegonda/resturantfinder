897f6c04f6390a5e341d5b8276bd48b63036c290
Puneeth Regonda <puneethregonda1291@gmail.com>
2024-12-08 20:13:01 -0800

    added frontend change for deployment

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,19 +1,26 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
-export default defineConfig({
-  plugins: [react()],
-  build: {
-    outDir: '../backend/src/main/webapp', // Build output directory matches the backend's static file location
-    emptyOutDir: true,
-  },
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:8080', // Backend URL during local dev
-        changeOrigin: true,
-        secure: false,
+export default defineConfig(({ mode }) => {
+  const isProduction = mode === 'production';
+
+  return {
+    plugins: [react()],
+    build: {
+      outDir: 'build', // Build output directory
+      // outDir: '../backend/src/main/webapp', // Build output directory matches the backend's static file location
+      emptyOutDir: true,
+    },
+    server: {
+      proxy: {
+        '/api': {
+          target: isProduction
+            ? 'http://3.82.244.78:8080' // Production backend URL
+            : 'http://localhost:8080', // Backend URL during local dev
+          changeOrigin: true,
+          secure: false,
+        },
       },
     },
-  },
+  };
 });
